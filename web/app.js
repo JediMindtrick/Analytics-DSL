4bfc81f1ad91037c4e2bbf67ebc62fa42d6fb4ab
Brandon Wilhite <brandon@dagger.(none)>
2013-05-18 06:58:11 -0400

    removed stupid Object.prototype.isObject, added jquery ui and knockout-jqueryui

--- web/app.js
+++ web/app.js
@@ -1,7 +1,7 @@
 require=(function(e,t,n){function i(n,s){if(!t[n]){if(!e[n]){var o=typeof require=="function"&&require;if(!s&&o)return o(n,!0);if(r)return r(n,!0);throw new Error("Cannot find module '"+n+"'")}var u=t[n]={exports:{}};e[n][0].call(u.exports,function(t){var r=e[n][1][t];return i(r?r:t)},u,u.exports)}return t[n].exports}var r=typeof require=="function"&&require;for(var s=0;s<n.length;s++)i(n[s]);return i})({"../lib/util.js":[function(require,module,exports){
 module.exports=require('cEM32v');
 },{}],"cEM32v":[function(require,module,exports){
-(function(){Object.prototype.isObject = true;
+(function(){//Object.prototype.isObject = true;
 Function.prototype.isFunction = true;
 String.prototype.isString = true;
 Number.prototype.isNumber = true;
@@ -68,199 +68,7 @@ exports.hasValue = _hasValue;
 exports.stringHasValue = _stringHasValue;
 exports.importModule = _importModule;
 })()
-},{}],"../lib/executionEngine.js":[function(require,module,exports){
-module.exports=require('8Cl3l8');
-},{}],"8Cl3l8":[function(require,module,exports){
-var u = require('./util');
-var getArgs = u.getArgs;
-var hasValue = u.hasValue;
-var op = require('./operations').op;
-
-var reportingLevel = 'verbose';
-var log = function(msg){
-	if(reportingLevel === 'verbose'){
-		console.log(msg);
-	}
-};
-
-var runtimeEngine = function(semanticModel){
-	this.model = semanticModel;
-	this.op = op;
-	this.currentTopSymbol = '';
-	this.currentEvalSymbol = '';
-	this.errorHandler = function(msg){};
-};
-
-runtimeEngine.prototype.subscribeToError = function(handler){
-	this.errorHandler = handler;
-};
-
-runtimeEngine.prototype.logError = function(msg){
-	if(this.errorHandler){
-		var toLog = {
-			topSymbol: this.currentTopSymbol,
-			evalSymbol: this.currentEvalSymbol,
-			message: msg
-		};
-
-		//add to outputs
-		if(!this.output[this.currentTopSymbol]){
-			this.output[this.currentTopSymbol] = {};
-		}
-		if(!this.output[this.currentTopSymbol].errors){
-			this.output[this.currentTopSymbol].errors = [];
-		}
-		this.output[this.currentTopSymbol].errors.push({
-			evalSymbol: this.currentEvalSymbol,
-			message: msg	
-		});
-
-		//notify
-		this.errorHandler(toLog);
-	}
-};
-
-var _addToOutput = function(topSymbol,output,tag,value){
-	if(!output[topSymbol]){
-		output[topSymbol] = {};
-	}
-	output[topSymbol][tag] = value;
-};
-
-runtimeEngine.prototype.execute = function(inputs){
-	this.inputs = inputs;
-	this.output = {};
-
-	for(var sym in inputs){
-
-		if(inputs.hasOwnProperty(sym)){
-
-			//for tracing and helpful error messages
-			this.currentTopSymbol = sym;
-
-			if(!this.model[this.currentTopSymbol]){
-				this.logError("Unhandled input identifier '" + this.currentTopSymbol + "'");
-				this.currentTopSymbol = '';
-
-				continue;
-			}
-
-			try{
-				this.evaluate(this.currentTopSymbol);
-
-				if(invalidOutputValue(this.output[this.currentTopSymbol].result)){
-					this.logError('Operation for ' + this.currentTopSymbol + ' failed to output a valid value!');
-				}
-
-			}catch(ex){
-				this.logError(ex);
-			}
-
-			this.currentTopSymbol = '';
-			this.currentEvalSymbol = '';
-		}
-	}
-
-	return this.output;
-};
-
-
-var invalidOutputValue = function(value){
-	return value === null || value === undefined || !value.isNumber;
-};
-
-runtimeEngine.prototype.evaluate = function(symOrValue){
-
-	//already been determined/calculated
-	if(this.output[symOrValue] && this.output[symOrValue].result){
-		this.currentEvalSymbol = '';
-		return this.output[symOrValue].result;
-	
-	//case primitive value, strings are used for enums
-	}else if(symOrValue.isNumber || (!this.model[symOrValue] && symOrValue.isString)){
-		this.currentEvalSymbol = '';
-		return symOrValue;
-	
-	//operation
-	}else if(symOrValue.isArray || this.model[symOrValue]){
-
-		var operation = null;
-
-		//grab the operation, which includes an operator as well as arguments
-		if(this.model[symOrValue]){
-			operation = this.model[symOrValue];
-			this.currentEvalSymbol = symOrValue;
-		}else {
-			operation = symOrValue;
-			this.currentEvalSymbol = this.op[operation[0]].Name;
-		}
-
-		var argsToPass = [];
-
-		//is an input value required?
-		if(this.op[operation[0]].RequiresInput && 
-			!hasValue(this.inputs[symOrValue])){
-			throw 'Input required for ' + symOrValue + ' in order to complete calculations.';
-		//if so, retrieve it
-		} else if (this.op[operation[0]].RequiresInput) {
-			argsToPass.push(this.inputs[symOrValue]);			
-		}
-
-		/*evaluate further arguments*/
-		var args = [];
-		if(operation.length > 1){
-			args = operation.slice(1);
-
-			for(var i = 0, l = args.length; i < l; i++){
-				var curr = args[i];
-				//ignore nulls, undefineds and enums
-				if(hasValue(curr)){
-
-					//not to eval
-					if(this.op[operation[0]].PassWithoutEval){
-						argsToPass.push(curr);
-					//or to eval?
-					}else{
-						argsToPass.push(this.evaluate(curr));
-					}
-				}
-			}
-		}
-
-		//last argument is the symbol or value, in case we need
-		//to do a look up from the operation		
-		argsToPass.push(symOrValue);
-
-		//finally, apply the function
-		var f = this.getFunction(operation[0]);
-		var toReturn = f.apply(this,argsToPass);
-
-		//if it's part of the expected calculations, add it
-		if(this.model[symOrValue] && symOrValue === this.currentTopSymbol){
-			//_addToOutput
-			_addToOutput(this.currentTopSymbol,this.output,'result',toReturn);
-		}
-
-		this.currentEvalSymbol = '';
-		return toReturn;
-
-	//case error
-	}else{
-		throw 'Unknown symbol of ' + symOrValue + ' was passed to evaluator!';
-	}
-
-	this.currentEvalSymbol = '';
-};
-
-runtimeEngine.prototype.getFunction = function(name){
-	return this.op[name].Func;
-};
-
-exports.Engine = runtimeEngine;
-exports.setReportingLevel = function(level){
-	reportingLevel = level;
-};
-},{"./util":"cEM32v","./operations":"FCLlNE"}],"../lib/operations.js":[function(require,module,exports){
+},{}],"../lib/operations.js":[function(require,module,exports){
 module.exports=require('FCLlNE');
 },{}],"FCLlNE":[function(require,module,exports){
 (function(){/* 
@@ -547,7 +355,199 @@ var op = {
 
 exports.op = op;
 })()
-},{"./util":"cEM32v"}],"../lib/builder.js":[function(require,module,exports){
+},{"./util":"cEM32v"}],"../lib/executionEngine.js":[function(require,module,exports){
+module.exports=require('8Cl3l8');
+},{}],"8Cl3l8":[function(require,module,exports){
+var u = require('./util');
+var getArgs = u.getArgs;
+var hasValue = u.hasValue;
+var op = require('./operations').op;
+
+var reportingLevel = 'verbose';
+var log = function(msg){
+	if(reportingLevel === 'verbose'){
+		console.log(msg);
+	}
+};
+
+var runtimeEngine = function(semanticModel){
+	this.model = semanticModel;
+	this.op = op;
+	this.currentTopSymbol = '';
+	this.currentEvalSymbol = '';
+	this.errorHandler = function(msg){};
+};
+
+runtimeEngine.prototype.subscribeToError = function(handler){
+	this.errorHandler = handler;
+};
+
+runtimeEngine.prototype.logError = function(msg){
+	if(this.errorHandler){
+		var toLog = {
+			topSymbol: this.currentTopSymbol,
+			evalSymbol: this.currentEvalSymbol,
+			message: msg
+		};
+
+		//add to outputs
+		if(!this.output[this.currentTopSymbol]){
+			this.output[this.currentTopSymbol] = {};
+		}
+		if(!this.output[this.currentTopSymbol].errors){
+			this.output[this.currentTopSymbol].errors = [];
+		}
+		this.output[this.currentTopSymbol].errors.push({
+			evalSymbol: this.currentEvalSymbol,
+			message: msg	
+		});
+
+		//notify
+		this.errorHandler(toLog);
+	}
+};
+
+var _addToOutput = function(topSymbol,output,tag,value){
+	if(!output[topSymbol]){
+		output[topSymbol] = {};
+	}
+	output[topSymbol][tag] = value;
+};
+
+runtimeEngine.prototype.execute = function(inputs){
+	this.inputs = inputs;
+	this.output = {};
+
+	for(var sym in inputs){
+
+		if(inputs.hasOwnProperty(sym)){
+
+			//for tracing and helpful error messages
+			this.currentTopSymbol = sym;
+
+			if(!this.model[this.currentTopSymbol]){
+				this.logError("Unhandled input identifier '" + this.currentTopSymbol + "'");
+				this.currentTopSymbol = '';
+
+				continue;
+			}
+
+			try{
+				this.evaluate(this.currentTopSymbol);
+
+				if(invalidOutputValue(this.output[this.currentTopSymbol].result)){
+					this.logError('Operation for ' + this.currentTopSymbol + ' failed to output a valid value!');
+				}
+
+			}catch(ex){
+				this.logError(ex);
+			}
+
+			this.currentTopSymbol = '';
+			this.currentEvalSymbol = '';
+		}
+	}
+
+	return this.output;
+};
+
+
+var invalidOutputValue = function(value){
+	return value === null || value === undefined || !value.isNumber;
+};
+
+runtimeEngine.prototype.evaluate = function(symOrValue){
+
+	//already been determined/calculated
+	if(this.output[symOrValue] && this.output[symOrValue].result){
+		this.currentEvalSymbol = '';
+		return this.output[symOrValue].result;
+	
+	//case primitive value, strings are used for enums
+	}else if(symOrValue.isNumber || (!this.model[symOrValue] && symOrValue.isString)){
+		this.currentEvalSymbol = '';
+		return symOrValue;
+	
+	//operation
+	}else if(symOrValue.isArray || this.model[symOrValue]){
+
+		var operation = null;
+
+		//grab the operation, which includes an operator as well as arguments
+		if(this.model[symOrValue]){
+			operation = this.model[symOrValue];
+			this.currentEvalSymbol = symOrValue;
+		}else {
+			operation = symOrValue;
+			this.currentEvalSymbol = this.op[operation[0]].Name;
+		}
+
+		var argsToPass = [];
+
+		//is an input value required?
+		if(this.op[operation[0]].RequiresInput && 
+			!hasValue(this.inputs[symOrValue])){
+			throw 'Input required for ' + symOrValue + ' in order to complete calculations.';
+		//if so, retrieve it
+		} else if (this.op[operation[0]].RequiresInput) {
+			argsToPass.push(this.inputs[symOrValue]);			
+		}
+
+		/*evaluate further arguments*/
+		var args = [];
+		if(operation.length > 1){
+			args = operation.slice(1);
+
+			for(var i = 0, l = args.length; i < l; i++){
+				var curr = args[i];
+				//ignore nulls, undefineds and enums
+				if(hasValue(curr)){
+
+					//not to eval
+					if(this.op[operation[0]].PassWithoutEval){
+						argsToPass.push(curr);
+					//or to eval?
+					}else{
+						argsToPass.push(this.evaluate(curr));
+					}
+				}
+			}
+		}
+
+		//last argument is the symbol or value, in case we need
+		//to do a look up from the operation		
+		argsToPass.push(symOrValue);
+
+		//finally, apply the function
+		var f = this.getFunction(operation[0]);
+		var toReturn = f.apply(this,argsToPass);
+
+		//if it's part of the expected calculations, add it
+		if(this.model[symOrValue] && symOrValue === this.currentTopSymbol){
+			//_addToOutput
+			_addToOutput(this.currentTopSymbol,this.output,'result',toReturn);
+		}
+
+		this.currentEvalSymbol = '';
+		return toReturn;
+
+	//case error
+	}else{
+		throw 'Unknown symbol of ' + symOrValue + ' was passed to evaluator!';
+	}
+
+	this.currentEvalSymbol = '';
+};
+
+runtimeEngine.prototype.getFunction = function(name){
+	return this.op[name].Func;
+};
+
+exports.Engine = runtimeEngine;
+exports.setReportingLevel = function(level){
+	reportingLevel = level;
+};
+},{"./util":"cEM32v","./operations":"FCLlNE"}],"../lib/builder.js":[function(require,module,exports){
 module.exports=require('SGnVB/');
 },{}],"SGnVB/":[function(require,module,exports){
 (function(){var u = require('./util');
